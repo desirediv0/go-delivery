24959b732dd5630808b7e58918844d85ba165edc
riteshk-007 <codeshorts007@gmail.com>
2025-02-21 15:01:09 +0530

    Remove commented-out code in Contact component for cleaner codebase

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -47,7 +47,6 @@ export default function Contact() {
       <div className="absolute inset-0 w-full h-full">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-cyan-300 rounded-full mix-blend-multiply filter blur-xl opacity-30 animate-blob" />
         <div className="absolute top-0 -right-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-30 animate-blob animation-delay-2000" />
-        {/* <div className="absolute -bottom-8 left-20 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-30 animate-blob animation-delay-4000" /> */}
       </div>
 
       <div className="container mx-auto px-4 relative z-10">
