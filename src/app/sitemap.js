f43e4fb5c42e2e548c6e77aa23d5657f2f82ffbf
riteshk-007 <codeshorts007@gmail.com>
2025-02-24 13:01:29 +0530

    Add robots and sitemap functionality for SEO optimization

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://godeliverygroup.com",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://godeliverygroup.com/about",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://godeliverygroup.com/contact",
+            lastModified: new Date(),
+        },
+    ];
+}
