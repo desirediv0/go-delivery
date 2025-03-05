9783dc81ac51bad7de2d550d4e48e52a29985a62
riteshk-007 <codeshorts007@gmail.com>
2025-03-05 17:39:19 +0530

    App replace to Website text

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -8,7 +8,7 @@ const PrivacyPolicy = () => {
 
                 <div className="bg-white p-6 md:p-8 space-y-6 text-gray-700">
                     <p className="font-medium">
-                        This Privacy Policy ("Privacy Policy") governs your use of websites, mobile applications and technology operated by GoDelivery Group.
+                        This Privacy Policy ("Privacy Policy") governs your use of website operated by GoDelivery Group.
                     </p>
 
                     <div className="space-y-4">
@@ -19,25 +19,25 @@ const PrivacyPolicy = () => {
                         <ul className="list-disc pl-6 space-y-2">
                             <li>GoDelivery Group network of websites, including www.godeliverygroup.com (including any versions optimized for viewing on a wireless or tablet device)</li>
                             <li>All email newsletters published or distributed by GoDelivery Group</li>
-                            <li>All apps published by GoDelivery Group, including the "GoDelivery" app</li>
+                            <li>All website published by GoDelivery Group, including the "GoDelivery" website</li>
                             <li>Any other services made available by GoDelivery Group ("Service")</li>
-                            <li>All other interactive features and communications provided by GoDelivery Group ("App"), however accessed and/or used that are operated by us made available by us, or produced and maintained by us, and our related companies (collectively "GoDelivery" or "we", "us", or "our")</li>
+                            <li>All other interactive features and communications provided by GoDelivery Group ("Website"), however accessed and/or used that are operated by us made available by us, or produced and maintained by us, and our related companies (collectively "GoDelivery" or "we", "us", or "our")</li>
                         </ul>
                     </div>
 
                     <div className="space-y-4">
                         <p>
-                            We have established this privacy policy ("Privacy Policy") to ensure that you understand what kinds of personal information we may gather during your use of this App, why we gather your information, how we use that information, when we might disclose your personal information, and how can you manage your personal information.
+                            We have established this privacy policy ("Privacy Policy") to ensure that you understand what kinds of personal information we may gather during your use of this Website, why we gather your information, how we use that information, when we might disclose your personal information, and how can you manage your personal information.
                         </p>
 
                         <p>
-                            Please note that the practices described in this Privacy Policy apply to information gathered online through our App and websites, and by our customer service personnel. It does not apply to information that you share with organizations to which we may link or who may link to us, or information that we may receive about you from other organizations.
+                            Please note that the practices described in this Privacy Policy apply to information gathered online through our  websites, and by our customer service personnel. It does not apply to information that you share with organizations to which we may link or who may link to us, or information that we may receive about you from other organizations.
                         </p>
                     </div>
 
                     <div className="space-y-4">
                         <p>
-                            By using our App, you accept the practices described in our Privacy Policy. If you do not agree to the terms of this Privacy Policy, please do not use the App. We reserve the right to revise the terms of our Privacy Policy from time to time without notice. Your continued use of our App following the changes to these terms will mean you accept those changes. If we intend to apply the changes to this Privacy Policy retroactively or to personal information already in our possession, we will notify you of the modifications or amendments.
+                            By using our Website, you accept the practices described in our Privacy Policy. If you do not agree to the terms of this Privacy Policy, please do not use the Website. We reserve the right to revise the terms of our Privacy Policy from time to time without notice. Your continued use of our Website following the changes to these terms will mean you accept those changes. If we intend to apply the changes to this Privacy Policy retroactively or to personal information already in our possession, we will notify you of the modifications or amendments.
                         </p>
 
                         <p>
@@ -53,7 +53,7 @@ const PrivacyPolicy = () => {
                         </p>
 
                         <p>
-                            The app uses Third Party services for user KYC via Aadhaar Number that may collect information used to identify you.
+                            The Website uses Third Party services for user KYC via Aadhaar Number that may collect information used to identify you.
                         </p>
                     </div>
 
@@ -61,21 +61,21 @@ const PrivacyPolicy = () => {
                         <h2 className="text-xl font-semibold text-gray-800">Personal Information</h2>
 
                         <p>
-                            You may browse the App without submitting any personal information. However, there are a number of features and Services that can be accessed only on submission of the required personal information. The most common ways in which we may collect your personal information are listed below.
+                            You may browse the Website without submitting any personal information. However, there are a number of features and Services that can be accessed only on submission of the required personal information. The most common ways in which we may collect your personal information are listed below.
                         </p>
 
                         <ul className="list-disc pl-6 space-y-2">
-                            <li>When you register for an account on the GoDelivery App</li>
-                            <li>When you use GoDelivery App to refer to your friends</li>
+                            <li>When you register for an account on the GoDelivery Website</li>
+                            <li>When you use GoDelivery Website to refer to your friends</li>
                             <li>On submission of payment information for adding money to your GoDelivery wallet</li>
-                            <li>App related communications, e.g. Aadhaar Number provided for KYC</li>
-                            <li>Profile information (personal and non-personal) provided by you in the GoDelivery App User Profile</li>
+                            <li>Website related communications, e.g. Aadhaar Number provided for KYC</li>
+                            <li>Profile information (personal and non-personal) provided by you in the GoDelivery Website User Profile</li>
                             <li>When you share your ride details on your social media authorized by you</li>
                             <li>When you participate in GoDelivery ratings, surveys, or promotion</li>
                             <li>When you sign up to receive alerts or information via email, text or instant messages from GoDelivery</li>
                             <li>When you request for assistance, customer service or support</li>
-                            <li>When you submit data or information on the App where it is permitted</li>
-                            <li>When you knowingly volunteer personal information anywhere on the App</li>
+                            <li>When you submit data or information on the Website where it is permitted</li>
+                            <li>When you knowingly volunteer personal information anywhere on the Website</li>
                         </ul>
                     </div>
 
@@ -83,15 +83,15 @@ const PrivacyPolicy = () => {
                         <h2 className="text-xl font-semibold text-gray-800">Log Data</h2>
 
                         <p>
-                            When you use our Service and if there is an error in the App, we collect data and information (through third party products) on your phone, called Log Data. This Log Data may include information such as your device Internet Protocol (IP) address of your server, device name, operating system version, the app configuration while you are using our service, the time and date when you use our service, the duration, and other statistics.
+                            When you use our Service and if there is an error in the Website, we collect data and information (through third party products) on your phone, called Log Data. This Log Data may include information such as your device Internet Protocol (IP) address of your server, device name, operating system version, the Website configuration while you are using our service, the time and date when you use our service, the duration, and other statistics.
                         </p>
 
                         <ul className="list-disc pl-6 space-y-2">
-                            <li>The device information about the computer, browser, mobile device or any other device that you use to access the App. This information may include the IP address, geolocation, unique device identifiers, type of browser, language, and other transactional information.</li>
-                            <li>When you use the App, a third party server or we might collect information about your use of the App, including GPS routes and status of GPS chips.</li>
+                            <li>The device information about the computer, browser, mobile device or any other device that you use to access the Website. This information may include the IP address, geolocation, unique device identifiers, type of browser, language, and other transactional information.</li>
+                            <li>When you use the Website, a third party server or we might collect information about your use of the Website, including GPS routes and status of GPS chips.</li>
                             <li>Device information about the GoDelivery, including time stamps and battery status.</li>
-                            <li>Additional "traffic data" and log files such as time of access, date of access, software crash reports, session identification number, access times, and referring App addresses.</li>
-                            <li>Other information regarding your use of the App.</li>
+                            <li>Additional "traffic data" and log files such as time of access, date of access, software crash reports, session identification number, access times, and referring Website addresses.</li>
+                            <li>Other information regarding your use of the Website.</li>
                         </ul>
                     </div>
 
@@ -107,17 +107,17 @@ const PrivacyPolicy = () => {
                         <h2 className="text-xl font-semibold text-gray-800">Cookies</h2>
 
                         <p>
-                            Cookies are files with small amount of data that is commonly used as anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory. This Service does not use these "cookies" explicitly. However, the app may use third party code and libraries that use "cookies" to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some features of this Service.
+                            Cookies are files with small amount of data that is commonly used as anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory. This Service does not use these "cookies" explicitly. However, the Website may use third party code and libraries that use "cookies" to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some features of this Service.
                         </p>
 
                         <p>We use cookies for other purposes as below:</p>
 
                         <ul className="list-disc pl-6 space-y-2">
                             <li>We use cookies to remind us who you are and to find your account information in our database when you access a Service so you do not need to log in at every visit. This helps us to provide you with services tailored to your particular needs and interests. A cookie is created when you register for a Service.</li>
-                            <li>We use cookies to determine the browser you use so the Apps can be designed to work efficiently with the most common versions of different browsers</li>
+                            <li>We use cookies to determine the browser you use so the Website can be designed to work efficiently with the most common versions of different browsers</li>
                             <li>We use cookies in conjunction with sending you e-mail newsletters</li>
-                            <li>Advertisers that place ads on the App may use cookies</li>
-                            <li>We use cookies to generate analytics regarding our App</li>
+                            <li>Advertisers that place ads on the Website may use cookies</li>
+                            <li>We use cookies to generate analytics regarding our Website</li>
                             <li>We use cookies to determine user traffic. Your browser is given a unique cookie that helps us determine whether yours is a repeat visit or a first visit</li>
                         </ul>
                     </div>
